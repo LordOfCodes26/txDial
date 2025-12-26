c800e34eb08b2209fe37a91f2720dbc20ccff1f2
LordOfCodes26 <dtao.bittensor@gmail.com>
2025-12-26 03:28:50 -0500

    fix FastScrollerView.kt

--- app/src/main/kotlin/com/android/dialer/extensions/FastScrollerView.kt
+++ app/src/main/kotlin/com/android/dialer/extensions/FastScrollerView.kt
@@ -4,16 +4,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.reddit.indicatorfastscroll.FastScrollItemIndicator
 import com.reddit.indicatorfastscroll.FastScrollerView
 import com.goodwy.commons.models.contacts.Contact
+import com.android.dialer.utils.CharUtils
 
 fun FastScrollerView.setupWithContacts(
     recyclerView: RecyclerView,
     contacts: List<Contact>,
 ) = setupWithRecyclerView(recyclerView, { position ->
-    val initialLetter = try {
-        contacts[position].getFirstLetter()
+    val sectionIndicator = try {
+        val contact = contacts[position]
+        val name = contact.getBubbleText()
+        
+        // Check if the name starts with a Korean character
+        if (name.isNotEmpty() && CharUtils.isKoreanCharacter(name[0])) {
+            // Use Korean consonant grouping for Korean characters
+            CharUtils.getFirstConsonant(name)
+        } else {
+            // Use the existing first letter method for non-Korean characters
+            contact.getFirstLetter()
+        }
     } catch (_: IndexOutOfBoundsException) {
         ""
     }
 
-    FastScrollItemIndicator.Text(initialLetter)
+    FastScrollItemIndicator.Text(sectionIndicator)
 })
